0c365893f1c2f319ca0525a9079c987f20db12b0
M2kura <teterheo@cvut.cz>
2024-12-02 22:26:00 +0100

    Clean the solution

--- 2/main.go
+++ 2/main.go
@@ -21,7 +21,7 @@ func getNumsSlice(str string) []int {
 	return nums
 }
 
-func safecheck(nums []int, firstTime bool) bool {
+func safecheck(nums []int, problemDampener bool) bool {
 	for i := 0; i < len(nums) - 1; i++ {
 		difference := nums[i] - nums[i+1]
 		if nums[0] < nums[1] {
@@ -29,11 +29,10 @@ func safecheck(nums []int, firstTime bool) bool {
 		}
 
 		if difference < 1 || difference > 3 {
-			if firstTime  {
-				nums1 := append(append([]int(nil), nums[:i]...), nums[i+1:]...)
-				nums2 := append(append([]int(nil), nums[:i+1]...), nums[i+2:]...)
-				if safecheck(nums1, false) || safecheck(nums2, false) {
-					return true
+			if problemDampener {
+				for j, _ := range nums {
+					sub_nums := append(append([]int(nil), nums[:j]...), nums[j+1:]...)
+					if (safecheck(sub_nums, false)) { return true }
 				}
 			}
 			return false
@@ -72,36 +71,11 @@ func main() {
 	res = 0
 	reader.Reset(fileToStr("input"))
 	scanner = bufio.NewScanner(reader)
-	file, _ := os.OpenFile("test", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	defer file.Close()
-	var wrong_nums [][]int
 
 	for scanner.Scan() {
 		nums := getNumsSlice(scanner.Text())
-		if safecheck(nums, true) {
-			res += 1
-		} else {
-			file.WriteString(fmt.Sprint(nums) + "\n")
-			wrong_nums = append(wrong_nums, nums)
-		}
+		if safecheck(nums, true) { res += 1 }
 	}
 
 	fmt.Println(res)
-
-	// I could just do this in the safecheck function and live a happy life
-	var valid_nums [][]int
-	for _, wrong_num := range wrong_nums {
-		for i, _ := range wrong_num {
-			slice := append(append([]int(nil), wrong_num[:i]...), wrong_num[i+1:]...)
-			if safecheck(slice, false) {
-				valid_nums = append(valid_nums, wrong_num)
-			}
-		}
-	}
-
-	file2, _ := os.OpenFile("test_new", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	defer file2.Close()
-	for _, nums := range valid_nums {
-		file2.WriteString(fmt.Sprint(nums) + "\n")
-	}
 }
